19071c210f8122fa43cb7a4c2a5f24da7707f83f
Sam <sam@dev.com>
2023-08-25 14:45:45 +0200

    WIP

---
+++ src/sqlitecloud/__init__.py
@@ -0,0 +1,5 @@
+from src.sqlitecloud.driver import SqliteCloudClient
+
+
+def get_sqlitecloud_client(connection_str:str) -> SqliteCloudClient:
+    pass
---
+++ src/sqlitecloud/driver.py
@@ -0,0 +1,20 @@
+from uuid import UUID
+import os
+import ctypes
+
+from sqlitecloud.wrapper_types import SQCloudConfig
+
+lib = ctypes.CDLL(os.getenv("SQLITECLOUD_DRIVER_PATH","libsqcloud.so"))
+connect = lib.SQCloudConnect
+
+SQCloudConnect = lib.SQCloudConnect
+SQCloudConnect.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.POINTER(SQCloudConfig)]
+SQCloudConnect.restype = ctypes.c_void_p  
+SQCloudIsError = lib.SQCloudIsError
+SQCloudIsError.argtypes = [ctypes.c_void_p]  # Assuming SQCloudConnection * is a void pointer
+SQCloudIsError.restype = ctypes.c_bool
+
+SQCloudErrorMsg = lib.SQCloudErrorMsg
+SQCloudErrorMsg.argtypes = [ctypes.c_void_p]  # Assuming SQCloudConnection * is a void pointer
+SQCloudErrorMsg.restype = ctypes.c_char_p
+
