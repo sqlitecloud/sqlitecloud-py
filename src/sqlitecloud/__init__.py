b11c47a7880350e516ce49140787284f67f6257b
Sam <sam@dev.com>
2023-09-05 19:37:06 +0200

    Bump version: 0.1.1 → 0.1.2

--- src/sqlitecloud/__init__.py
+++ src/sqlitecloud/__init__.py
@@ -1,5 +1,5 @@
 from sqlitecloud.client import SqliteCloudClient
-version='0.1.1'
+version='0.1.2'
 
 def get_sqlitecloud_client(connection_str: str) -> SqliteCloudClient:
     print(connection_str)
