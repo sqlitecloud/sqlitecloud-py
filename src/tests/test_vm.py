89f40f613d96c4dc7217b07769b5553010ba0b0e
matteofredi <m.fredi@codermine.com>
2023-12-01 16:07:13 +0100

    [test] implemented step_vm method's test

--- src/tests/test_vm.py
+++ src/tests/test_vm.py
@@ -1,6 +1,6 @@
 from sqlitecloud.client import SqliteCloudClient, SqliteCloudAccount
 from sqlitecloud.conn_info import user, password, host, db_name, port
-from sqlitecloud.vm import compile_vm
+from sqlitecloud.vm import compile_vm, step_vm
 
 
 def test_compile_vm():
@@ -8,4 +8,13 @@ def test_compile_vm():
     client = SqliteCloudClient(cloud_account=account)
     conn = client.open_connection()
     compile_vm(conn, "INSERT INTO people (emp_name) VALUES (?1);")
+    client.disconnect(conn)
+
+
+def test_step_vm():
+    account = SqliteCloudAccount(user, password, host, db_name, port)
+    client = SqliteCloudClient(cloud_account=account)
+    conn = client.open_connection()
+    vm = compile_vm(conn, "INSERT INTO people (emp_name) VALUES (?1);")
+    step_vm(vm)
     client.disconnect(conn)
