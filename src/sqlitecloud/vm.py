aa6690865a16cb7cc9288699988da0637aa71e28
matteofredi <m.fredi@codermine.com>
2023-12-01 16:06:57 +0100

    [feature] implemented step_vm method

--- src/sqlitecloud/vm.py
+++ src/sqlitecloud/vm.py
@@ -1,8 +1,11 @@
 import ctypes
 
-from experiments.test_ctypes import SQCloudConnect
-from sqlitecloud.driver import SQCloudVM, SQCloudVMCompile
+from sqlitecloud.driver import SQCloudConnect, SQCloudVM, SQCloudVMCompile, SQCloudVMStep
 
 
 def compile_vm(conn: SQCloudConnect, query: str) -> SQCloudVM:
     return SQCloudVMCompile(conn, ctypes.c_char_p(query.encode("utf-8")), -1, None)
+
+
+def step_vm(vm: SQCloudVMCompile) -> int:
+    return SQCloudVMStep(vm)
