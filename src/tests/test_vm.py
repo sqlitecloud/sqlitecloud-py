07e74e6c187ec2a5c1dd7bdd2d32b3e64b710172
matteofredi <m.fredi@codermine.com>
2023-12-06 08:43:27 +0100

    [feature] implemented result_vm's unit test

--- src/tests/test_vm.py
+++ src/tests/test_vm.py
@@ -1,13 +1,13 @@
 from sqlitecloud.client import SqliteCloudClient, SqliteCloudAccount
 from sqlitecloud.conn_info import user, password, host, db_name, port
-from sqlitecloud.vm import compile_vm, step_vm
+from sqlitecloud.vm import compile_vm, step_vm, result_vm
 
 
 def test_compile_vm():
     account = SqliteCloudAccount(user, password, host, db_name, port)
     client = SqliteCloudClient(cloud_account=account)
     conn = client.open_connection()
-    compile_vm(conn, "INSERT INTO people (emp_name) VALUES (?1);")
+    compile_vm(conn, "INSERT INTO employees (emp_name) VALUES (?1);")
     client.disconnect(conn)
 
 
@@ -17,6 +17,17 @@ def test_step_vm():
     conn = client.open_connection()
     vm = compile_vm(conn, "INSERT INTO employees (emp_name) VALUES (?1);")
     result = step_vm(vm)
+    client.disconnect(conn)
+
+    assert isinstance(result, int), type(result)
+
+
+def test_result_vm():
+    account = SqliteCloudAccount(user, password, host, db_name, port)
+    client = SqliteCloudClient(cloud_account=account)
+    conn = client.open_connection()
+    vm = compile_vm(conn, "INSERT INTO employees (emp_name) VALUES (?1);")
+    result = result_vm(vm)
     client.disconnect(conn)
 
     assert isinstance(result, int), type(result)
