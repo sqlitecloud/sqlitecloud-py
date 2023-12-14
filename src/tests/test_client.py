b2592249d619ca0037f9bf661e1c005e8f3d2a87
matteofredi <m.fredi@codermine.com>
2023-12-14 10:41:24 +0100

    [test] close connection in test_client.py

--- src/tests/test_client.py
+++ src/tests/test_client.py
@@ -37,8 +37,10 @@ def test_sqlite_cloud_error_query():
     is_error = False
     try:
         client.exec_query(query, conn)
+        client.disconnect(conn)
     except Exception:
         is_error = True
+        client.disconnect(conn)
     assert is_error
 
 
@@ -53,6 +55,7 @@ def test_sqlite_cloud_float_agg_query():
     first_element = next(result)
     assert "result" in first_element.keys()
     print("Float result", first_element)
+    client.disconnect(conn)
 
 
 def test_sqlite_cloud_int_agg_query():
@@ -66,3 +69,4 @@ def test_sqlite_cloud_int_agg_query():
     first_element = next(result)
     print("Int result", first_element)
     assert "result" in first_element.keys()
+    client.disconnect(conn)
