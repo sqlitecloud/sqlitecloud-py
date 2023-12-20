c13ec3042fbef99a80a5ad3be67ee99d58a1a24e
matteofredi <m.fredi@codermine.com>
2023-12-20 14:54:38 +0100

    Updated version & github url

--- src/setup.py
+++ src/setup.py
@@ -18,12 +18,12 @@ def read_file(filename):
 
 setup(
     name='SqliteCloud',
-    version='0.1.10',
+    version='0.0.1',
     author='Sam Reghenzi & Matteo Fredi',
     description='A Python package for working with SQLite databases in the cloud.',
     long_description=read_file('README-PYPI.md'),
     long_description_content_type='text/markdown',
-    url="https://github.com/codermine/sqlitecloud-python-sdk",
+    url="https://github.com/sqlitecloud/python",
     packages=find_packages(),
     install_requires=[
         'mypy == 1.6.1',
